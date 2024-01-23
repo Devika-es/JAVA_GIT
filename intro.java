2a43c91780581d9550b2963c00e1397e0f768dd9
Devika E S <devikaes17@gmail.com>
2024-01-23 19:36:12 +0530

    switch_case

--- intro.java
+++ intro.java
@@ -1,15 +1,32 @@
 //create two variables performabove mentioned operations and print the result
 
-public class intro {
+//  class intro {
 
-    public static void main(String[] args)
+//     public static void main(String args[])
+//     {
+//         int a=5;
+//         int b=10;
+//         System.out.println("Addition->"+ (a+b));
+//         System.out.println("Subtract->" + (a-b));
+//         System.out.println("Multiply->"+ (a*b));
+//         System.out.println("Divide->"+ (a/b));
+//         System.out.println("Modulus->"+(a%b));
+//     }
+// }
+
+ public class intro {
+    public static void main(String args[])
     {
-        int a=5;
-        int b=10;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
+        int a=-5;
+        if(a<0)
+        {
+            System.out.println(-1*a);
+        }
+        else
+        {
+            System.out.println(a);
+        }
+       
     }
 }
+
