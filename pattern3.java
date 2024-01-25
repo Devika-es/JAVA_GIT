e54d4a8129371f92a7c7f63b8ed2d4f0536f00b5
Devika E S <devikaes17@gmail.com>
2024-01-25 23:51:14 +0530

    number_pattern

---
+++ pattern3.java
@@ -0,0 +1,14 @@
+public class pattern3 {
+    public static void main(String args[])
+    {
+        for(int i=1;i<=4;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}
