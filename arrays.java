bcdae68854490e2979019cf61686ca9d9ae51d17
Devika E S <devikaes17@gmail.com>
2024-01-29 19:55:03 +0530

    array_intro

---
+++ arrays.java
@@ -0,0 +1,10 @@
+public class arrays {
+    public static void main(String[] args)
+    {
+        int arr[]={1,2,3,4,5};//declaration
+        System.out.println(arr[3]);
+        System.out.println(arr[0]);
+
+    }
+    
+}
