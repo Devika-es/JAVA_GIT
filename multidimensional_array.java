d98181f5bacbbe67494eb318c8eb434f108ef8c5
Devika E S <devikaes17@gmail.com>
2024-01-30 20:58:44 +0530

    2D_array

---
+++ multidimensional_array.java
@@ -0,0 +1,76 @@
+
+//print border elements
+
+// public class multidimensional_array {
+//     public static void main(String[] args)
+//     {
+//         int arr[][]={{1,2,3},{4,5,6},{7,8,9},{0,6,1}};
+//         for(int row=0;row<arr.length;row++)
+//         {
+//             for(int column=0;column<arr[0].length;column++)
+//             {
+//                 if(row==0||row==arr.length-1||column==0||column==arr[0].length-1){
+//                 System.out.print(arr[row][column]+" ");
+//                 }
+//                 else{
+//                     System.out.print("  ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+//print all the elements below diagonal elements
+// public class multidimensional_array {
+//     public static void main(String[] args)
+//     {
+//         int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+//         for(int row=0;row<arr.length;row++)
+//         {
+//             for(int column=0;column<arr[0].length;column++)
+//             {
+//                 if(row>=column)
+//                 {
+//                     System.out.print(arr[row][column]+" ");
+//                 }
+               
+    
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+//print even elements
+// public class multidimensional_array {
+//     public static void main(String[] args)
+//     {
+//         int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+//         for(int row=0;row<arr.length;row++)
+//         {
+//             for(int column=0;column<arr[0].length;column++)
+//             {
+//                if(arr[row][column]%2==0)
+//                 {
+//                     System.out.print(arr[row][column]+" ");
+//                 }
+//                 else{
+//                     System.out.print("  ");
+//                 }
+    
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+//swap first row with last row of the array
+//replace all the negative elements of an array with 0
+//find the maximum element in the array and print
+//sum of each row in an array,columns,diagonals
+
