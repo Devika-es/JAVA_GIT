3d14f7529300f65ce2d9f617700b2b319e3f1218
Devika E S <devikaes17@gmail.com>
2024-01-30 12:32:59 +0530

    advanced_pattern

---
+++ newpattern.java
@@ -0,0 +1,49 @@
+public class newpattern {
+    public static void main(String args[])
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=5;j>i;j--)
+            {
+                System.out.print("*");
+            }
+            for(int k=0;k<(2*i);k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=5;j>i;j--)
+            {
+                    System.out.print("*");
+            }
+            if(i!=4)
+            {
+                System.out.print("\n");
+            }
+        }
+           
+        for(int l=0;l<=5;l++)
+        {
+            for(int m=0;m<l;m++)
+            {
+                System.out.print("*");
+            }
+            for(int n=5;n>l;n--)
+            {
+                System.out.print(" ");
+            }
+            for(int n=5;n>l;n--)
+            {
+                System.out.print(" ");
+            }
+            for(int m=0;m<l;m++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        
+        
+    
+    }
+    
+}
--- trial.java
+++ trial.java
@@ -1,3 +1,20 @@
 public class trial {
-    
+    public static void main(String[] args)
+    {
+
+        // float arr[]={13.2f,45.6f,78.9f,95.4f,66.5f};
+        // float sum=0;
+       
+        // for(int i=0;i<arr.length;i++)
+        // {        
+        //     sum=sum+arr[i];    
+        // }
+        // System.out.println(sum);
+        
+        
+        int arr[][]=new int[5][4];//declaration
+        
+
+     
+    }
 }
