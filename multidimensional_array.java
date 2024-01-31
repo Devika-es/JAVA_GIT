6b922d7835e795e4755753ed14330cf1a53f4bec
Devika E S <devikaes17@gmail.com>
2024-01-31 19:26:58 +0530

    jagged_array.

--- multidimensional_array.java
+++ multidimensional_array.java
@@ -74,3 +74,98 @@
 //find the maximum element in the array and print
 //sum of each row in an array,columns,diagonals
 
+
+//replace all the negative elements of an array with 0
+// public class multidimensional_array {
+//     public static void main(String[] args)
+//     {
+//         int arr[][]={{1,2,-3},{-4,5,6},{7,-8,9}};
+//        for(int row=0;row<arr.length;row++)
+//        {
+//         for(int column=0;column<arr[0].length;column++)
+//         {
+//             if(arr[row][column]<0)
+//             {
+//                 System.out.print(0+" ");
+//             }
+//             else
+//             {
+//                 System.out.print(arr[row][column]+" ");
+//             }
+//         }
+//         System.out.println();
+//        }
+//     }
+// }
+
+
+
+// sum of each row in an array
+// public class multidimensional_array {
+//     public static void main(String[] args)
+//     {
+//         int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+//         int sum=0;
+//        for(int row=0;row<arr.length;row++)
+//        {
+//         for(int column=0;column<arr[0].length;column++)
+//         {
+//             sum=sum+arr[row][column];    
+//         }
+//         System.out.print("Sum of each row "+sum);
+//         sum=0;
+//         System.out.println();
+//        }
+//     }
+// }
+
+
+//find the maximum element in the array and print
+// public class multidimensional_array {
+//     public static void main(String[] args)
+//     {
+//         int arr[][]={{11,2,3},{4,5,6},{7,8,9}};
+//         int a=arr[0][0];
+//         int max_element=0;
+//        for(int row=0;row<arr.length;row++)
+//        {
+//         for(int column=0;column<arr[0].length;column++)
+//         {
+//             if(a<arr[row][column])
+//             {
+//                 max_element=arr[row][column];
+//             }
+//             else{
+//                 max_element=a;
+//             }         
+//         }
+//        }
+//        System.out.print("Largest number in array "+ max_element);
+//     }
+// }
+
+
+
+//swap first row with last row of the array
+// public class multidimensional_array{
+//     public static void main(String[] args)
+//     {
+//         int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+//         for(int row=0;row<arr.length;row++)
+//         {
+//             for(int column=0;column<arr[0].length;column++)
+//             {
+//                 int temp=arr[0][column];
+//                 arr[0][column]=arr[arr.length-1][column];
+//                 arr[arr.length-1][column]=temp;
+//                 if(row==0||row==arr.length-1)
+//                 {
+//                     System.out.print(arr[row][column]+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
