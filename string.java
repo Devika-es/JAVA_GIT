162ffc8a7ced58d0c26ea6c1933dd2c4a8eacf59
Devika E S <devikaes17@gmail.com>
2024-02-13 19:44:43 +0530

    strings_intro

---
+++ string.java
@@ -0,0 +1,12 @@
+public class string {
+    public static void main(String args[])
+    {
+        String firstname="hello";
+        String lastname="World";
+        String d1="hello";
+        System.out.println(firstname==lastname);
+        System.out.println(d1==firstname);
+        System.out.println(d1.charAt(0));
+        System.out.println(lastname.length());
+    }
+}
