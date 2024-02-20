01546a694b163a78e208aa210de94db863bda959
Devika E S <devikaes17@gmail.com>
2024-02-20 19:53:29 +0530

    aggregation

---
+++ aggregation.java
@@ -0,0 +1,42 @@
+class Address
+{
+    String city;
+    String state;
+    public Address(String city,String state)
+    {
+        this.city=city;
+
+        this.state=state;
+    }
+}
+
+
+class Student
+{
+    int id;
+    String name;
+    Address address;
+    public Student(int id,String name,Address address)
+    {
+        this.id=id;
+        this.name=name;
+        this.address=address;
+    }
+
+    void display()
+    {
+        System.out.println("Id: "+ id +" | "+"Name: "+name+"| Address: "+address.state);
+    }
+}
+
+
+
+
+public class aggregation {
+    public static void main(String args[])
+    {
+        Address a1=new Address("City","State");
+        Student s1=new Student(1,"John",a1);
+        s1.display();
+    }
+}
