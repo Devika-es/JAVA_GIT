257bc9d2c3aaef80196b6ea542b856ed6bb9e2bf
Your Name <devikaes17@gmail.com>
2025-03-27 22:38:10 +0530

    reverse a string using recursion

---
+++ ReverseString1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ReverseString1 {
+    public static String reverse(String str)
+    {
+        if(str.isEmpty())
+        {
+            return str;
+        }
+        return reverse(str.substring(1))+str.charAt(0);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.print(reverse(str));
+    }
+}
